6ec857716260e2f0ec78a40142643e9c342afc50
Bhargavi784 <bhargavideshmukh784@gmail.com>
2024-08-13 09:44:10 +0530

    Add files via upload

---
+++ Web Dev/exp3.js
@@ -0,0 +1,12 @@
+document.getElementById('generateBtn').addEventListener('click', generatePassword);
+
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById('password').value = password;
+}
